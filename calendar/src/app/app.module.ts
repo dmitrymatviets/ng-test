ac4ce45088dca03a8f3065df962d97afc2f7a4bc
matviets <dmitriy.matviets@goods.ru>
2018-09-27 12:21:37 +0300

    popover

--- calendar/src/app/app.module.ts
+++ calendar/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { Calendar } from './calendar/calendar';
+import { Calendar } from './ui/calendar/calendar';
 
 @NgModule({
   declarations: [
